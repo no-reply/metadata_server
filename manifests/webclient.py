c995b8e4ba22b73e824b0c694e880b6a890d308b
Chip Goines <chip_goines@harvard.edu>
2015-03-24 03:01:31 -0400

    urllib client wrapper

---
+++ manifests/webclient.py
@@ -0,0 +1,10 @@
+/usr/bin/python
+
+import urllib2
+
+def get(url, cookie_value=None):
+    opener = urllib2.build_opener()
+    if cookie_value:
+        opener.addheaders.append(('Cookie', 'hulaccess='+cookie_value))
+    response = opener.open(url)
+    return response
