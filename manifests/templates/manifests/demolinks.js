04b8fc22f1c9641bc81114f6d306ca46e6fe56e5
Chip Goines <chip_goines@harvard.edu>
2015-04-17 14:31:26 -0400

    Edits to demo links.js
    added Lyon, David Gordon, 1852-1935. Papers of David Gordon Lyon, 1861, 1871-1935.
    added olvwork411464 HUAM182040
    fixed huam182040
    added drs:48660497
    fixed huam18200
    fixed hls
    added 2 more hollis links

--- manifests/templates/manifests/demolinks.js
+++ manifests/templates/manifests/demolinks.js
@@ -12,4 +12,12 @@
 { "manifestUri": "http://iiif.lib.harvard.edu/manifests/drs:21791087", "location": "Harvard University", "title": "Griffin, Samuel, 1762-1812. Mathematical manuscript of Samuel Griffin, 1783-1784. HUC 8784.353, Harvard University Archives"},
 { "manifestUri": "http://iiif.lib.harvard.edu/manifests/via:olvwork279002", "location": "Harvard University", "title": "United States. Army. Chaplain School. 8th Graduating Class Chaplains School, Harvard University, Cambridge, Massachusetts, 1943. HUPSF Chaplain School PA (1), Harvard University Archives"},
 { "manifestUri": "http://iiif.lib.harvard.edu/manifests/hollis:010890132", "location": "Harvard University", "title": "Müller, Th. Panorama de Paris : vue prise place de la Concorde. (Paris : Imp. Lemercier & Cie., [1867]), G5834.P3A3 1867 .M8, Harvard Map Collection"},
-{ "manifestUri": "http://purl.stanford.edu/zw200wd8767/iiif/manifest.json", "location": "Stanford University", "title": "Walters Ms. W.34, Carrow Psalter"}
+{ "manifestUri": "http://purl.stanford.edu/zw200wd8767/iiif/manifest.json", "location": "Stanford University", "title": "Walters Ms. W.34, Carrow Psalter"},
+{ "manifestUri": "http://iiif.lib.harvard.edu/manifests/drs:47567736", "location": "Harvard University", "title": "Lyon, David Gordon, 1852-1935. Papers of David Gordon Lyon, 1861, 1871-1935. Vol. 1"},
+{ "manifestUri": "http://iiif.lib.harvard.edu/manifests/drs:47584795", "location": "Harvard University", "title": "Lyon, David Gordon, 1852-1935. Papers of David Gordon Lyon, 1861, 1871-1935. Diary ephemera, 1870-1871"},
+{ "manifestUri": "http://iiif.lib.harvard.edu/manifests/drs:47567737", "location": "Harvard University", "title": "Lyon, David Gordon, 1852-1935. Papers of David Gordon Lyon, 1861, 1871-1935. Diary, 1870-1871 Vol. 2"},
+{ "manifestUri": "http://iiif.lib.harvard.edu/manifests/via:HUAM182040", "location": "Harvard University", "title": "Cuneiform Inscribed Clay Nail: Early Dynastic IIIb Foundation Inscription of Entemena of Lagash"},
+{ "manifestUri": "http://iiif.lib.harvard.edu/manifests/via:olvwork411464", "location": "Harvard University", "title": "Cuneiform tablet"},
+{ "manifestUrl": "http://iiif.lib.harvard.edu/manifests/drs:48660497", "location": "Harvard University", "title": "HLS MS 184, Harvard Law School Library. Statuta vetera, registrum brevium and tracts, late 13th or early 14th century"},
+{ "manifestUrl": "http://iiif.lib.harvard.edu/manifests/hollis:14019043", "location": "Harvard University", "title": ""},
+{ "manifestUrl": "http://iiif.lib.harvard.edu/manifests/hollis:011426680", "location": "Harvard University", "title": ""},
