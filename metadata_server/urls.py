649a4fc80df2784f18235dec63314fe475bbddca
Dave Mayo <dave_mayo@harvard.edu>
2015-04-21 13:52:44 -0400

    Added route to demo from root

--- metadata_server/urls.py
+++ metadata_server/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from manifests import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^manifests/', include('manifests.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^demo$', views.demo, name="demo"),
 )
