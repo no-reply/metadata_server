d6073bba591e077aa2a8b8567c17e21bea2bdba5
Dave Mayo <dave_mayo@harvard.edu>
2015-05-11 13:20:47 -0400

    Possible parser fix for sequences, testing on Sheeler.

--- manifests/mets.py
+++ manifests/mets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from lxml import etree
-import json, sys
+import json, sys, re
 import urllib2
 from django.conf import settings
 import webclient
@@ -43,7 +43,7 @@ def get_display_image(fids):
         """Goes through list of file IDs for a page, and returns the best choice for delivery (according to mime hierarchy)."""
 
         def proc_fid(out, fid):
-                """Internal fn mapped over all images. Sets first image of each mime-type in out hash."""
+                """Internal fn mapped over all images. Sets last image of each mime-type in out hash."""
                 img = imageHash.get(fid, [])
                 if len(img) == 2:
                         out[img["mime"]] = (img["img"], fid)
@@ -60,10 +60,7 @@ def get_display_image(fids):
 
 def process_page(sd, rangeKey, new_ranges):
 	# first check if PAGE has label, otherwise get parents LABEL/ORDER
-	if 'LABEL' in sd.attrib:
-		label = sd.get('LABEL') + " (seq. {0})".format(sd.get("ORDER"))
-	else:
-		label = rangeKey + " (seq. {0})".format(sd.get("ORDER"))
+        label = get_rangeKey(sd)
 
         display_image, fid = get_display_image(sd.xpath('./mets:fptr/@FILEID', namespaces=XMLNS))
 
@@ -75,24 +72,71 @@ def process_page(sd, rangeKey, new_ranges):
 				canvasInfo.append(info)
                                 range = {}
                                 range[label] = imageHash[fid]["img"]
-                                new_ranges.append(range)
 
-def process_intermediate(subdivs, rangeKey):
+        return range
+
+def is_page(div):
+        return 'TYPE' in div.attrib and div.get('TYPE') == 'PAGE'
+
+# Regex for determining if page number exists in label
+page_re = re.compile(r"[pP](?:age|\.) \[?(\d+)\]?")
+
+# RangeKey used for table of contents
+# Logic taken from pds/**/navsection.jsp, cleaned up for redundant cases
+#
+# Note: Doesn't use page_num because it does deduplication
+#       in cases where ORDERLABEL is represented in LABEL
+def get_rangeKey(div):
+        if is_page(div):
+                label = div.get('LABEL', "").strip()
+                oLabel = div.get('ORDERLABEL', "").strip()
+                seq = div.get('ORDER')
+                seq_s = "(seq. {0})".format(seq)
+                if label:
+                        match = page_re.match(label)
+                        pn_from_label = match and match.group(1)
+                # Both label and orderlabel exist and are not empty
+                if div.get('LABEL') and div.get('ORDERLABEL'):
+                        # ORDERLABEL duplicates info in LABEL
+                        if pn_from_label or oLabel.lower() in label.lower():
+                                return "{0} {1}".format(label, seq_s)
+                        else:
+                                return "{0}, p. {1} {2}".format(label, pn_from_label, seq_s)
+                elif not label and oLabel:
+                        return "p. {0} {2}".format(oLabel, seq_s)
+                elif label and not oLabel:
+                        return "{0} {1}".format(label, seq_s)
+                else:
+                        return seq_s
+        # Intermediates
+	else:
+                label = div.get('LABEL', "").strip()
+                f, l = get_intermediate_seq_values(div[0], div[-1])
+                display_ss = ""
+                if f["page"] and l["page"]:
+                        if f["page"] == l["page"]:
+                                display_ss = "p. {0} ".format(f["page"])
+                        else:
+                                display_ss ="pp. {0}-{1} ".format(f["page"], l["page"])
+
+                return label + \
+                        display_ss + \
+                        "(seq. {0})".format(f["seq"]) if f["seq"] == l["seq"] else "(seq. {0}-{1})".format(f["seq"], l["seq"])
+
+def process_intermediate(subdivs, rangeKey, new_ranges=None):
         """Processes intermediate divs in the structMap.
         Side effect: stores seq_statements for intermediate divs as part of operation."""
 
-        new_ranges = []
-
-        # Generate a sequence statement for subdiv
-        f,l = get_intermediate_seq_values(subdivs[0], subdivs[-1])
-        seq_statements[rangeKey] = "(seq. {0})".format(f) if f == l else "(seq. {0}-{1})".format(f,l)
+        new_ranges = new_ranges or []
 
         for sd in subdivs:
                 # leaf node, get canvas info
-                if 'TYPE' in sd.attrib and sd.get("TYPE") == 'PAGE':
-                        process_page(sd, rangeKey, new_ranges)
+                if is_page(sd):
+                        p_range = process_page(sd, rangeKey, new_ranges)
+                        if p_range:
+                                new_ranges.append(p_range)
                 else:
-                        new_ranges.extend(process_struct_map(sd, []))
+                        new_ranges.extend(process_intermediate_divs(sd, get_rangeKey(sd)))
         # this is for the books where every single page is labeled (like Book of Hours)
         # most books do not do this
         if len(new_ranges) == 1:
@@ -101,28 +145,39 @@ def process_intermediate(subdivs, rangeKey):
 
         return new_ranges
 
+
+# Get page number from ORDERLABEL or, failing that, LABEL, or, failing that, return None
+def page_num(div):
+        if 'ORDERLABEL' in div.attrib:
+                return div.get('ORDERLABEL')
+        else:
+                match = page_re.match(div.get('LABEL', ''))
+                return match and match.group(1)
+
 def get_intermediate_seq_values(first, last):
-        """Gets bookend values for constructing (seq. N-M) range display."""
+        """Gets bookend values for constructing pp. and seq. range display, e.g. pp. 8-9 (seq. 10-17)."""
+
+        # Drill down to first page
+        while first.get('TYPE') == 'INTERMEDIATE':
+                first = first[0]
 
         if first.get('TYPE') == 'PAGE':
-                first_val = first.get('ORDER')
-        else:
-                first_val, _ = get_intermediate_seq_values(first[0],first[-1])
+                first_vals = {"seq": first.get('ORDER'), "page": page_num(first)}
+
+        # Drill down last page
+        while last.get('TYPE') == 'INTERMEDIATE':
+                last = last[-1]
 
         if last.get('TYPE') == 'PAGE':
-                last_val = last.get('ORDER')
-        else:
-                _, last_val = get_intermediate_seq_values(last[0], last[-1])
+                last_vals = {"seq": last.get('ORDER'), "page": page_num(last)}
 
-        return first_val, last_val
+        return first_vals, last_vals
 
-def process_struct_map(div, ranges):
-	if 'LABEL' in div.attrib:
-		rangeKey = div.get('LABEL')
-	else:
-		rangeKey = div.get('ORDER')
+def process_struct_divs(div, ranges):
+        """Toplevel processing function.  Run over contents of the CITATION div (or the stitched subdiv if present)."""
+	rangeKey = get_rangeKey(div)
 
-	# when the top level divs are PAGEs
+	# when the top level div is a PAGE
 	if 'TYPE' in div.attrib and div.get("TYPE") == 'PAGE':
 		new_ranges = []
 		process_page(div, rangeKey, new_ranges)
@@ -130,12 +185,23 @@ def process_struct_map(div, ranges):
 			range_dict = new_ranges[0]
 			new_ranges = range_dict.get(range_dict.keys()[0])
 		ranges.append({rangeKey : new_ranges})
+        else:
+                subdivs = div.xpath('./mets:div', namespaces = XMLNS)
+                if len(subdivs) > 0:
+                        new_ranges = process_intermediate(subdivs, get_rangeKey(div))
+                        ranges.append({rangeKey : new_ranges})
 
-	subdivs = div.xpath('./mets:div', namespaces = XMLNS)
-	if len(subdivs) > 0:
-                new_ranges = process_intermediate(subdivs, rangeKey)
-		ranges.append({rangeKey : new_ranges})
 	return ranges
+
+def process_structMap(smap):
+        divs = smap.xpath('./mets:div[@TYPE="CITATION"]/mets:div', namespaces=XMLNS)
+
+        # Check if the object has a stitched version(s) already made.  Use only those
+        for st in divs:
+                stitchCheck = st.xpath('./@LABEL[contains(., "stitched")]', namespaces=XMLNS)
+                if stitchCheck:
+                        divs = st
+                        break
 
 def get_leaf_canvases(ranges, leaf_canvases):
 	for range in ranges:
@@ -171,7 +237,7 @@ def create_range_json(ranges, manifest_uri, range_id, within, label):
 	rangesJsonList.append(rangejson)
 
 def create_ranges(ranges, previous_id, manifest_uri):
-	if not any(isinstance(x, dict) for x in ranges):
+        if not any(isinstance(x, dict) for x in ranges):
 		return
 
 	counter = 0
@@ -189,6 +255,7 @@ def create_ranges(ranges, previous_id, manifest_uri):
 		create_ranges(new_ranges, range_id, manifest_uri)
 
 def main(data, document_id, source, host, cookie=None):
+
 	# clear global variables
 	global imageHash
 	imageHash = {}
@@ -266,7 +333,7 @@ def main(data, document_id, source, host, cookie=None):
 	rangeList = []
 	rangeInfo = []
 	for st in struct:
-		ranges = process_struct_map(st, [])
+		ranges = process_struct_divs(st, [])
 		rangeList.extend(ranges)
 	if len(rangeList) > 1:
 		rangeInfo = [{"Table of Contents" : rangeList}]
@@ -293,8 +360,8 @@ def main(data, document_id, source, host, cookie=None):
 
 	canvases = []
 	for cvs in canvasInfo:
-		response = webclient.get(imageUriBase + cvs['image'] + imageInfoSuffix, cookie)
-		infojson = json.load(response)
+                response = webclient.get(imageUriBase + cvs['image'] + imageInfoSuffix, cookie)
+                infojson = json.load(response)
 
                 if "gif" in infojson['formats']:
                         fmt = "image/gif"
