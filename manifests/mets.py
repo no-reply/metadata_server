3ece1b3ebe15cdc648b2938375479d03dbb85cf1
Dave Mayo <dave_mayo@harvard.edu>
2015-05-14 09:27:22 -0400

    Might not be labeled right on books of hours (if the label is on the intermediate), but should parse correctly

--- manifests/mets.py
+++ manifests/mets.py
@@ -140,9 +140,8 @@ def process_intermediate(div, new_ranges=None):
 
         # this is for the books where every single page is labeled (like Book of Hours)
         # most books do not do this
-        if len(new_ranges) == 1 and len(new_ranges[0].values()[0]) == 1:
-                sub_dict = new_ranges[0]
-                new_ranges = range_dict.values()[0].values()[0]
+        if len(new_ranges) == 1:
+                return new_ranges[0]
 
         return {get_rangeKey(div): new_ranges}
 
